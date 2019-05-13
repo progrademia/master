dad5271d86a13ffa781d7a1884a48822e9e452e6
Progrademia <hola@progrademia.com>
2019-05-13 12:48:55 +0200

    Estructura de un bloque PL/SQL

---
+++ OracleSQL/20190513-2.sql
@@ -0,0 +1,18 @@
+-- Módulos
+-- Integración
+-- Portabilidad
+-- Excepciones
+
+-- Estructura bloque PL/SQL
+
+DECLARE /*OPCIONAL*/
+-- VARIABLES, CURSORES
+
+BEGIN --obligatorio
+
+-- sentencias SQL y PL/SQL
+EXCEPTION --(opcionales)
+
+END; --obligatorio
+
+
